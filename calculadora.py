308fa88fa006732b6fc083e065c6cb602f2610d1
Óscar quintana ciprés <91286218+florinato@users.noreply.github.com>
2023-09-25 15:38:19 +0200

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,139 +1,80 @@
-from tkinter import *
+from tkinter import Tk, Frame, Entry, Button, StringVar
 
 
-global a
-global op
-a=""
-op=""
-aux=""
-raiz=Tk()
-raiz.title("Calculadora Basica")
-miframe=Frame(raiz)
+class SimpleCalculator:
+    """Clase que implementa una calculadora básica con una interfaz gráfica de usuario."""
 
-
-raiz.geometry('400x250')
-miframe.pack()
-
-numeropantalla=StringVar()
-pantalla=Entry(miframe,textvariable=numeropantalla,font=("Roboto Cn",14))
-pantalla.grid(row=0 , column=1 , columnspan=8)
-pantalla.config(background="black" , fg="#03f943" , justify="right")
-numeropantalla.set("0.0")
-
-
-def numeropulsado(num):
-  
-
-    global  a
-    if num == "+":
-        operaciones(num)
+    def __init__(self, master):
+        """
+        Inicializa la calculadora.
         
-    elif num == "-":
-        operaciones(num)
-            
-    elif num=="=":
-        operaciones(num)
-    elif num=="C":
-        numeropantalla.set("0.0")
+        Args:
+        - master (Tk): La ventana principal de la aplicación.
+        """
+        self.master = master
+        self.master.title("Calculadora Básica")
+        self.master.geometry('400x250')
+        self.current_input = ""
+        self.operation = ""
+        self.previous_input = ""
+        self.display = StringVar()
+        self.display.set("0.0")
         
-    elif num=="*":
-        operaciones(num)
+        self.initialize_ui()
+
+    def initialize_ui(self):
+        """Configura y muestra los widgets de la interfaz de usuario."""
+        miframe = Frame(self.master)
+        miframe.pack()
+
+        entry = Entry(miframe, textvariable=self.display, font=("Roboto Cn", 14))
+        entry.grid(row=0, column=1, columnspan=8)
+        entry.config(background="black", fg="#03f943", justify="right")
+
+        buttons = [
+            ("C", 1, 4), ("7", 2, 1), ("8", 2, 2), ("9", 2, 3), ("/", 2, 4),
+            ("4", 3, 1), ("5", 3, 2), ("6", 3, 3), ("*", 3, 4),
+            ("1", 4, 1), ("2", 4, 2), ("3", 4, 3), ("-", 4, 4),
+            ("0", 5, 1), (".", 5, 2), ("=", 5, 3), ("+", 5, 4)
+        ]
+
+        for (text, row, col) in buttons:
+            button = Button(
+                miframe, text=text, width=8, font=("Roboto Cn", 14),
+                command=lambda t=text: self.button_pressed(t)
+            )
+            button.grid(row=row, column=col)
+
+    def button_pressed(self, button_text):
+        """
+        Maneja los eventos de presión de botón.
         
-    elif num=="/":
-        operaciones(num)
-        
+        Args:
+        - button_text (str): El texto del botón que fue presionado.
+        """
+        if button_text in "0123456789.":
+            self.current_input += button_text
+        elif button_text in "+-*/":
+            self.previous_input = self.current_input
+            self.operation = button_text
+            self.current_input = ""
+        elif button_text == "=":
+            if self.operation and self.previous_input:
+                try:
+                    self.current_input = str(eval(f"{self.previous_input}{self.operation}{self.current_input}"))
+                except ZeroDivisionError:
+                    self.current_input = "ERROR"
+            self.operation = ""
+            self.previous_input = ""
+        elif button_text == "C":
+            self.current_input = ""
+            self.operation = ""
+            self.previous_input = ""
 
-    else:
-        a= a+num
-        numeropantalla.set(a)
-
-
-def operaciones(num):
-    
-    global a
-    global op
-    global aux
-    
-    if num=="+":
-        aux=numeropantalla.get()
-        op= num
-        a=""
-    elif num=="-":
-        aux=numeropantalla.get()
-        op= num
-        a=""
-    elif num=="C":
-        a=""
-        op=""
-        numeropantalla.set("0")
-    elif num=="/":
-        aux=numeropantalla.get()
-        op= num
-        a=""
-    elif num=="*":
-        aux=numeropantalla.get()
-        op= num
-        a=""
-    else:
-
-        numeropantalla.set(operacion(op,aux))
-        a=""
-        op=""   
-        aux=""  
-
-def operacion(op,aux):
-    try:
-        
-       
-        if op=="":
-            resultado=numeropantalla.get()
-
-        elif op=="+":
-            resultado=float(aux)+float(numeropantalla.get())
-        
-        elif op=="-":
-            resultado=float(aux)-float(numeropantalla.get())
-        
-        elif op=="/":
-            resultado=float(aux)/float(numeropantalla.get())
-        
-        elif op=="*":
-            resultado=float(aux)*float(numeropantalla.get())
-    except:
-        resultado="ERROR"
+        self.display.set(self.current_input or "0.0")
 
 
-    return(resultado)
-
-    
-    
-botones = ("C",1,4,"7",2,1,"8",2,2,"9",2,3,"/",2,4,"4",3,1,"5",3,2,
-"6",3,3,"*",3,4,"1",4,1,"2",4,2,"3",4,3,"-",4,4,"0",5,1,".",5,2,"=",5,3,"+",5,4)
-botones_iter = iter(botones)
-def botones():
-    b=next(botones_iter)   
-    e=next(botones_iter)  
-    o=next(botones_iter)   
-    boton=Button(miframe,text=b,width=8,font=("Roboto Cn",14),command=lambda:numeropulsado(b))
-    boton.grid(row=e,column=o)
-    return(boton)
-boton1=botones()
-boton2=botones()
-boton3=botones()
-boton4=botones()
-boton5=botones()
-boton6=botones()
-boton7=botones()
-boton8=botones()
-boton9=botones()
-boton10=botones()
-boton11=botones()
-boton12=botones()
-boton13=botones()
-boton14=botones()
-boton15=botones()
-boton16=botones()
-boton17=botones()
-
-
-raiz.mainloop()
+if __name__ == "__main__":
+    root = Tk()
+    SimpleCalculator(root)
+    root.mainloop()
